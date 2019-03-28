12ce34b1272897a5207b4767dfc84955b68ea576
Thomas Claxton <claxtons4@gmail.com>
2019-03-28 12:38:30 -0500

    Saving interval to chrome.storage.sync, defaults to 00:30

--- options.js
+++ options.js
@@ -1,4 +1,23 @@
-let interval = document.getElementById('intervalDiv');
-chrome.storage.sync.set( { interval: interval }, function() {
-    console.log('interval is ' + interval)
+let button = document.getElementById('submit');
+
+chrome.storage.sync.get({ minutes: '00', seconds: '30' }, function(settings) 
+{
+    console.log('Current interval: ' + settings.minutes + ':' + settings.seconds);
 })
+
+button.addEventListener('click', function() 
+{
+    let minuteField = document.getElementById('minuteField').value;
+    let secondField = document.getElementById('secondField').value;
+
+    if (minuteField == '') minuteField = '00';
+    if (secondField == '') secondField = '30';
+
+    // definitely some work to do on handling input
+
+    chrome.storage.sync.set( { minutes: minuteField, seconds: secondField }, function() 
+    {
+        alert('Interval set to ' + minuteField + ':' + secondField);
+        console.log('minutes: ' + minuteField + ' seconds: ' + secondField);
+    });
+});
