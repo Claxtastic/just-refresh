e40b108ca9a7873a233f0ca4553541d579526d93
Thomas Claxton <claxtons4@gmail.com>
2019-04-10 17:11:17 -0500

    Refreshing on interval, but refreshing one last time after toggled off

--- background.js
+++ background.js
@@ -2,30 +2,29 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function reload(ms) {
-  var toggleOff = false;
+var reload = function(ms, TOGGLE) {
   chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
     console.log('Waiting ' + ms + ' miliseconds for next refresh');
-    chrome.tabs.reload(tabs[0].id);
-    while (!toggleOff) 
-    {
+    while (TOGGLE) {
       await sleep(ms);
-      console.log(ms + ' miliseconds later');
-      chrome.browserAction.onClicked.addListener(function(tabs)
-        {
-          toggleOff = true;
-        });
+      if (TOGGLE) {
+        console.log(ms + ' miliseconds later');
+        chrome.tabs.reload(tabs[0].id);
+      }
     }
-  });
+  }); 
 }
 
 chrome.browserAction.onClicked.addListener(function(tabs) 
 {
-  chrome.storage.sync.get({ minutes: '00', seconds: '30' }, function(settings) 
+  chrome.storage.sync.get({ minutes: '00', seconds: '30' }, async function(settings) 
   {
+    var TOGGLE = false;
+    TOGGLE = !TOGGLE;
     let minutes = settings.minutes;
     let seconds = settings.seconds;
     let ms = (settings.minutes / 1000) + (settings.seconds * 1000); 
-    reload(ms)
+    await reload(ms, TOGGLE);
+    // else remove it from a global list of reloading tabs?
   });
 });
