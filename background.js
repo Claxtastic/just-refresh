b4428d65b31e2532392938f077c4d0b8c900c26b
Thomas Claxton <claxtons4@gmail.com>
2019-04-25 18:31:23 -0500

    Refactored background page-- now looping as intended. Added very basic style sheet for options page.

--- background.js
+++ background.js
@@ -1,30 +1,61 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+let refreshers = {};
 
-var reload = function(ms, TOGGLE) {
-  chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
-    console.log('Waiting ' + ms + ' miliseconds for next refresh');
-    while (TOGGLE) {
-      await sleep(ms);
-      if (TOGGLE) {
-        console.log(ms + ' miliseconds later');
-        chrome.tabs.reload(tabs[0].id);
-      }
-    }
-  }); 
-}
-
-chrome.browserAction.onClicked.addListener(function(tabs) 
+// Clicked extension icon
+chrome.browserAction.onClicked.addListener(function(tab) 
 {
-  chrome.storage.sync.get({ minutes: '00', seconds: '30' }, async function(settings) 
+  // Get stored interval; default to 00:30 if none
+  chrome.storage.sync.get({ minutes: '00', seconds: '30' }, function(settings) 
   {
-    var TOGGLE = false;
-    TOGGLE = !TOGGLE;
-    let minutes = settings.minutes;
-    let seconds = settings.seconds;
-    let ms = (settings.minutes / 1000) + (settings.seconds * 1000); 
-    await reload(ms, TOGGLE);
-    // else remove it from a global list of reloading tabs?
+    let ms = (settings.minutes * 60 * 1000) + (settings.seconds * 1000); 
+    let strId = String(tab.id);
+    if (refreshers.hasOwnProperty(strId)) {
+      // This tab is already has a refresher; remove it
+      deleteRefresher(refreshers, strId);
+    } else {
+      // This tab has no refresher; create one
+      createRefresher(refreshers, strId, ms);
+    }
   });
 });
+
+// Refreshing tab was closed; remove it from refreshers
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  try {
+    clearInterval(refreshers[String(tabId)].refresh);
+    delete refreshers[String(tabId)];
+  } catch (typeError) {
+    console.log('Caught ' + typeError);
+  }
+});
+
+function createRefresher(refreshers, strId, ms) {
+  // Get a readable version of the interval, as a string in seconds
+  let sec = String(ms / 1000);
+
+  let refresh = setInterval(function() {
+    if (refreshers[strId].tick > 0) {
+      // Tab is at least one second away from refreshing; decrement the tick and set the badgeText
+      sec = String(parseInt(refreshers[strId].tick--));
+      chrome.browserAction.setBadgeText({text: sec, tabId: parseInt(strId)});
+    } else {
+      // Tab is ready to refresh; assign tick to the interval and refresh
+      refreshers[strId].tick = refreshers[strId].interval;
+      chrome.tabs.reload(parseInt(strId));
+    }
+  }, 1000);
+
+  // Place refresher data for this tab in object
+  refreshers[strId] = {
+    "interval" : sec,
+    "tick" : sec,
+    "refresh" : refresh
+  };
+}
+
+function deleteRefresher(refreshers, strId) {
+  // Remove from this tab's data from refreshers
+  chrome.browserAction.setBadgeText({text: "", tabId: parseInt(strId)});
+  // Stop refresh loop
+  clearInterval(refreshers[strId].refresh);
+  delete refreshers[strId];
+}
