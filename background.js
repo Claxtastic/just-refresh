8d86d4390a1874335366d9a32aff1e040db78bb0
claxtastic <claxtons4@gmail.com>
2020-12-03 00:23:06 -0600

    WIP UI work

--- background.js
+++ background.js
@@ -1,90 +1,90 @@
-let refreshers = {};
+let refreshers = {}
 
 // Clicked extension icon
 chrome.browserAction.onClicked.addListener(function(tab) 
 {
-  createOrDeleteRefresher(String(tab.id));
-});
+  createOrDeleteRefresher(String(tab.id))
+})
 
 // Command toggle keybind pressed
 chrome.commands.onCommand.addListener(function(command) {
   // Because this is a command we don't have a tab as the callback parm, so query for active tab
   chrome.tabs.query({active: true}, function(tabs) {
-    createOrDeleteRefresher(String(tabs[0].id));
-  });
-});
+    createOrDeleteRefresher(String(tabs[0].id))
+  })
+})
 
-// Refreshing tab was closed; remove it from refreshers
+// Refreshing tab was closed remove it from refreshers
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
   try {
-    clearInterval(refreshers[String(tabId)].refresh);
-    delete refreshers[String(tabId)];
+    clearInterval(refreshers[String(tabId)].refresh)
+    delete refreshers[String(tabId)]
   } catch (typeError) {
-    console.log('Caught ' + typeError);
+    console.log('Caught ' + typeError)
   }
-});
+})
 
 function createOrDeleteRefresher(strId) {
-  // Get stored interval; default to 01:00 if none
+  // Get stored interval default to 01:00 if none
   chrome.storage.sync.get({ minutes: '01', seconds: '00' }, function(settings) 
   {
-    let ms = (settings.minutes * 60 * 1000) + (settings.seconds * 1000); 
+    let ms = (settings.minutes * 60 * 1000) + (settings.seconds * 1000) 
     if (refreshers.hasOwnProperty(strId)) {
-      // This tab is already has a refresher; remove it
-      deleteRefresher(refreshers, strId);
+      // This tab is already has a refresher remove it
+      deleteRefresher(refreshers, strId)
     } else {
-      // This tab has no refresher; create one
-      createRefresher(refreshers, strId, ms);
+      // This tab has no refresher create one
+      createRefresher(refreshers, strId, ms)
     }
-  });
+  })
 }
 
 function getMMSS(ms) {
   // create Date object for milliseconds, and return a nicely formatted mm:ss string
-  let date = new Date(ms);
-  var mm = String(date.getMinutes());
-  var ss = String(date.getSeconds());
+  let date = new Date(ms)
+  var mm = String(date.getMinutes())
+  var ss = String(date.getSeconds())
   if (date.getMinutes() < 10) {
     // pad minutes < 10 with a 0
-    mm = '0' + mm;
+    mm = '0' + mm
   } if (date.getSeconds() < 10) {
     // pad seconds < 10 with a 0
-    ss = '0' + ss;
+    ss = '0' + ss
   }
-  return mm + ':' + ss;
+  return mm + ':' + ss
 }
 
 function createRefresher(refreshers, strId, ms) {
   // Get a readable version of the interval, as a string in seconds
-  // let totalSeconds = String(ms / 1000);
-  let sec = String(ms / 1000);
+  // let totalSeconds = String(ms / 1000)
+  let sec = String(ms / 1000)
 
   let refresh = setInterval(function() {
     if (refreshers[strId].tick > 0) {
-      // Tab is at least one second away from refreshing; decrement the tick and set the badgeText
-      sec = String(parseInt(refreshers[strId].tick--));
-      var mmss = getMMSS(sec * 1000);
+      // Tab is at least one second away from refreshing decrement the tick and set the badgeText
+      sec = String(parseInt(refreshers[strId].tick--))
+      var mmss = getMMSS(sec * 1000)
 
-      chrome.browserAction.setBadgeText({text: mmss, tabId: Number(strId)});
+      chrome.browserAction.setBadgeText({text: mmss, tabId: Number(strId)})
     } else {
-      // Tab is ready to refresh; assign tick to the interval and refresh
-      refreshers[strId].tick = refreshers[strId].interval;
-      chrome.tabs.reload(parseInt(strId));
+      // Tab is ready to refresh assign tick to the interval and refresh
+      refreshers[strId].tick = refreshers[strId].interval
+      chrome.tabs.reload(parseInt(strId))
     }
-  }, 1000);
+  }, 1000)
 
   // Place refresher data for this tab in object
   refreshers[strId] = {
     "interval" : sec,
     "tick" : sec,
     "refresh" : refresh
-  };
+  }
 }
 
 function deleteRefresher(refreshers, strId) {
   // Remove from this tab's data from refreshers
-  chrome.browserAction.setBadgeText({text: "", tabId: parseInt(strId)});
+  chrome.browserAction.setBadgeText({text: "", tabId: parseInt(strId)})
   // Stop refresh loop
-  clearInterval(refreshers[strId].refresh);
-  delete refreshers[strId];
+  clearInterval(refreshers[strId].refresh)
+  delete refreshers[strId]
 }
--- options.js
+++ options.js
@@ -1,52 +1,64 @@
-let submit = document.getElementById('submit');
+let submit = document.getElementById('submit')
 let addNewButton = document.getElementById('addNewSite')
-let minuteField = document.getElementById('minuteField');
-let secondField = document.getElementById('secondField');
+let minuteField = document.getElementById('minuteField')
+let secondField = document.getElementById('secondField')
 
-chrome.storage.sync.get({ minutes: '01', seconds: '00' }, function(settings) 
+chrome.storage.sync.get({ minutes: '01', seconds: '00'}, function(settings) 
 {
-    minuteField.setAttribute('placeholder', settings.minutes);
-    secondField.setAttribute('placeholder', settings.seconds);
-    console.log('Current interval: ' + settings.minutes + ':' + settings.seconds);
+    minuteField.setAttribute('placeholder', settings.minutes)
+    secondField.setAttribute('placeholder', settings.seconds)
+    console.log('Current interval: ' + settings.minutes + ':' + settings.seconds)
+    chrome.storage.sync.get("perSiteSettings", function(value) {
+        console.log('Retrieved per site stetings: ', value)
+    })
 })
 
 submit.addEventListener('click', function() {
     // check for blank inputs
-    if (minuteField.value == '') minuteField.value = '01';
-    if (secondField.value == '') secondField.value = '00';
+    if (minuteField.value == '') minuteField.value = '01'
+    if (secondField.value == '') secondField.value = '00'
 
     // pad with 0's if single digit
-    if (minuteField.value.length < 2) minuteField.value = '0' + minuteField.value;
-    if (secondField.value.length < 2) secondField.value = '0' + secondField.value;
+    if (minuteField.value.length < 2) minuteField.value = '0' + minuteField.value
+    if (secondField.value.length < 2) secondField.value = '0' + secondField.value
 
-    chrome.storage.sync.set( { minutes: minuteField.value, seconds: secondField.value }, function() 
+    var perSiteSettings = {}
+    $('#tbody > tr').each(function() {
+        let website = $(this).find(".website").val()
+        let siteMinutes = $(this).find(".siteMinuteField").val()
+        let siteSeconds = $(this).find(".siteSecondField").val()
+        perSiteSettings[website] = { minutes: siteMinutes, seconds: siteSeconds }
+    })
+
+    chrome.storage.sync.set({ minutes: minuteField.value, seconds: secondField.value, perSiteSettings: perSiteSettings }, function() 
     {
-        alert('Interval set to ' + minuteField.value + ':' + secondField.value);
-        console.log('minutes: ' + minuteField.value + ' seconds: ' + secondField.value);
+        alert('Interval set to ' + minuteField.value + ':' + secondField.value)
+        console.log('minutes: ' + minuteField.value + ' seconds: ' + secondField.value)
+        console.log("Persite map: ", perSiteSettings)
     })
 })
 
-
 addNewButton.addEventListener('click', function() {
-    var table = document.getElementById('per-site-table')
-    var currentIndex = table.rows.length
-    var currentRow = table.insertRow(-1)
+    let table = document.getElementById('per-site-table')
+    let currentRow = table.insertRow(-1)
 
-    var websiteField = document.createElement('input')
+    let websiteField = document.createElement('input')
     websiteField.setAttribute('placeholder', 'https://www.google.com')
     websiteField.setAttribute('class', 'website')
 
-    var minuteField = document.createElement('input')
+    let minuteField = document.createElement('input')
+    minuteField.setAttribute('class', 'siteMinuteField')
     minuteField.setAttribute('placeholder', '01')
 
-    var colonElement = document.createElement('div')
+    let colonElement = document.createElement('div')
     colonElement.innerHTML = ":"
     colonElement.setAttribute('class', 'colon')
 
-    var secondField = document.createElement('input')
+    let secondField = document.createElement('input')
+    secondField.setAttribute('class', 'siteSecondField')
     secondField.setAttribute('placeholder', '00')
 
-    var currentCell = currentRow.insertCell(-1)
+    let currentCell = currentRow.insertCell(-1)
     currentCell.appendChild(websiteField)
 
     currentCell = currentRow.insertCell(-1)
